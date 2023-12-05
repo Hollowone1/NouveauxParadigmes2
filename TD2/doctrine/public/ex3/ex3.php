b854abc312d0c90d283696c01da515a550dac606
tamourgh1u <grindmoney19@gmail.com>
2023-12-05 15:12:56 +0100

    categoriere^p

---
+++ TD2/doctrine/public/ex3/ex3.php
@@ -0,0 +1,9 @@
+<?php
+
+use catadoct\catalog\Produit as Produit;
+use catadoct\catalog\Categorie;
+use catadoct\catalog\Taille;
+use catadoct\catalog\Tarif;
+
+require_once "../ormBootstrap.php";
+
--- TD2/doctrine/src/CategorieRepository.php
+++ TD2/doctrine/src/CategorieRepository.php
@@ -2,11 +2,23 @@
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityRepository;
+
 class CategorieRepository extends EntityRepository
 {
-    public function getProduits(string $keyword): Collection
+    public function getProduitsbycategorie(string $keyword): Collection
     {
+        $dql = "SELECT p FROM \\doctrine\src\Produit p
+        WHERE p.categorie ";
+        $query= $this->getEntityManager()->createQuery($dql);
+        return $query->getResult();
+    }
 
+    public function gettarifbyproducts(string $keyword): Collection
+    {
+        $dql = "SELECT t FROM \\doctrine\src\Tarif t
+        WHERE t.produit ";
+        $query= $this->getEntityManager()->createQuery($dql);
+        return $query->getResult();
     }
 
 }
